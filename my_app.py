f0b1509b8bc7dc106ece2f410c83ad73c3defa87
illya.petrusenko <illya.petrusenko@gmail.com>
2018-07-30 20:10:46 +0300

    from work -4

--- my_app.py
+++ my_app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>PHP-GAVno</h1>'
+    return render_template('index.html')
 
+
+@app.route('/monitorings')
+def monitorings():
+    return render_template('monitorings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
